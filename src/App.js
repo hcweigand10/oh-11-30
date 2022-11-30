8526856fb819a11611a8760ae874e1d857f8d05a
hcweigand10 <henryweigand10@gmail.com>
2022-11-30 13:59:40 -0800

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>1st Number: </h3>
+      <button>Incremet</button>
+      <h3>2nd Number: </h3>
+      <button>Incremet</button>
+      <br/>
+      <h3>Operation: </h3>
+      <select title="operation">
+        <option value="add">Add</option>
+        <option value="subtract">Subtract</option>
+        <option value="multiply">Multiply</option>
+        <option value="divide">Divide</option>
+      </select>
+      <h2>Answer: </h2>
     </div>
   );
 }
